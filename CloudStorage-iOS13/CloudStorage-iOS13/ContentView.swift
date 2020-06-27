7be1ddef0dd32c67f7d1dd50f8a18b77180c95a8
Tom Lokhorst <tom@lokhorst.eu>
2020-06-27 21:00:25 +0200

    Initial Commit

---
+++ CloudStorage-iOS13/CloudStorage-iOS13/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CloudStorage-iOS13
+//
+//  Created by Tom Lokhorst on 2020-07-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
