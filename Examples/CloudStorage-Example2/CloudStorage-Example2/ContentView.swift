aebf23cb6cc661f20506e9647e887d84a4af213f
Tom Lokhorst <tom@lokhorst.eu>
2023-04-20 12:42:37 +0200

    Add second example project

---
+++ Examples/CloudStorage-Example2/CloudStorage-Example2/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  Untitled 1
+//
+//  Created by Nonstrict on 2023-04-20.
+//
+
+import SwiftUI
+import CloudStorage
+
+struct ContentView: View {
+
+
+    var body: some View {
+        VStack {
+            TopView()
+            Divider()
+            MiddleView()
+            Divider()
+            BottomView()
+        }
+        .padding()
+    }
+}
+
+struct TopView: View {
+    @CloudStorage("myBool") var myBool = false
+
+    var body: some View {
+        Toggle("My Bool", isOn: $myBool)
+    }
+}
+
+struct MiddleView: View {
+    @CloudStorage("myBool") var myBool = false
+
+    var body: some View {
+        Toggle("My Bool", isOn: $myBool)
+    }
+}
+
+struct BottomView: View {
+    @StateObject var viewModel = ViewModel()
+
+    var body: some View {
+        Toggle("My Bool", isOn: $viewModel.myBool)
+
+        Button("Toggle") { viewModel.myBool.toggle() }
+    }
+}
+
+class ViewModel: ObservableObject {
+    @CloudStorage("myBool") var myBool = false
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
